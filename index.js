daa746c1307533c121dbfb279d1ac686388195d2
keithowino <designsolutions1629@gmail.com>
2024-03-19 09:30:06 +0300

    Clear comments.

--- index.js
+++ index.js
@@ -5,16 +5,3 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
-
-// let welcomeMessage = currentUser.toUpperCase() + " Welcome to Flatbook, !";
-
-// /*
-
-//     onst firstInitial = currentUser[0];
-//       const restOfName = currentUser.slice(1);
-
-// */
-
-// const firstInitial = currentUser[0];
-// const restOfName = currentUser.slice(1);
-// let shortGreeting = "Welcome, " + firstInitial + " " + restOfName;
