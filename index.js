9add7dc5b92f9637d79e913caf1e630d7796ded1
keithowino <designsolutions1629@gmail.com>
2024-03-19 09:28:32 +0300

    Complete Lab.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code in this file!
+
+const currentUser = "Dan Lee";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+// let welcomeMessage = currentUser.toUpperCase() + " Welcome to Flatbook, !";
+
+// /*
+
+//     onst firstInitial = currentUser[0];
+//       const restOfName = currentUser.slice(1);
+
+// */
+
+// const firstInitial = currentUser[0];
+// const restOfName = currentUser.slice(1);
+// let shortGreeting = "Welcome, " + firstInitial + " " + restOfName;
